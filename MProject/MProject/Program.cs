02127ddc498c0e0c14468fcf80f56adb2d08d078
wjtht <587401@qq.com>
2019-04-24 16:17:57 +0800

    add

---
+++ MProject/MProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsynchronousSocketListener.OnConn += AsynchronousSocketListener_OnConn;
+            AsynchronousSocketListener.OnReceive += AsynchronousSocketListener_OnReceive;
+            AsynchronousSocketListener.OnClose += AsynchronousSocketListener_OnClose;
+            AsynchronousSocketListener.Port = 8900;
+            AsynchronousSocketListener.StartListening();
+        }
+
+        private static void AsynchronousSocketListener_OnClose(StateObject state)
+        {
+            Console.WriteLine(state.SocketID + ":Offline");
+        }
+
+        private static void AsynchronousSocketListener_OnReceive(StateObject state, string msg)
+        {
+            Console.WriteLine(state.SocketID + "=>" + msg);
+        }
+
+        private static void AsynchronousSocketListener_OnConn(StateObject state)
+        {
+            Console.WriteLine(state.SocketID + ":Online");
+            AsynchronousSocketListener.Send(state.workSocket, state.SocketID);
+        }
+    }
+}
---
+++ MProject/SocketClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        public static SuperSocket.ClientEngine.AsyncTcpSession client = new SuperSocket.ClientEngine.AsyncTcpSession();
+        static void Main(string[] args)
+        {
+            client.Closed += Client_Closed;
+            client.Connected += Client_Connected;
+            client.DataReceived += Client_DataReceived;
+            client.Error += Client_Error;
+            new Thread(() => { Conn(); }).Start();
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Conn()
+        {
+            client.Connect(new IPEndPoint(IPAddress.Parse("192.168.192.99"), 8900));
+        }
+
+        private static void Client_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+        {
+            Conn();
+        }
+
+        private static void Client_DataReceived(object sender, SuperSocket.ClientEngine.DataEventArgs e)
+        {
+            Console.WriteLine("MSG:" + Encoding.ASCII.GetString(e.Data,0,e.Length));
+        }
+
+        private static void Client_Connected(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connected Server");
+        }
+
+        private static void Client_Closed(object sender, EventArgs e)
+        {
+            Conn();
+        }
+    }
+}
